de26c7f8d003f2bbeb7cca5581b5fadfa12c2689
Tim Håkansson <hakansson.tim@hotmail.com>
2016-08-24 18:59:10 +0200

    Cleanup commit

---
+++ gitcommit.bat
@@ -0,0 +1,32 @@
+@ECHO OFF
+
+git add *.lib
+git add *.lib
+git add *.idb
+git add *.pdb
+git add *.cpp
+git add *.h
+git add *.png
+git add *.jpg
+git add .gitignore
+git add *.c
+git add *.vcxproj
+git add *.filters
+git add *.user
+git add *.frag
+git add *.vert
+git add *.txt
+git add *.sln
+git add *.ttf
+git add *.suo
+git add *.dll
+git add *.bat
+git add */Source/*
+git add */include/*
+git add *COPYING
+git add *.gobj
+git add *.tsntodo
+git add *.wav
+git add *.so
+git add .gitconfig
+git status
