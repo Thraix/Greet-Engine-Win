8cf1b9dfecb8206d11c7e5caa767eca26c0747d9
Tim Håkansson <Tim Hkansson>
2016-11-01 09:11:32 +0100

    Added error messages in managers and mesh

--- Greet-core/src/audio/soundmanager.cpp
+++ Greet-core/src/audio/soundmanager.cpp
@@ -17,6 +17,15 @@ namespace greet{ namespace audio {
 
 	void SoundManager::add(audio::Sound* sound)
 	{
+		for (int i = 0;i < m_sounds.size();i++)
+		{
+			if (sound->getName() == m_sounds[i]->getName())
+			{
+				utils::ErrorHandle::setErrorCode(GREET_ERROR_MANAGER_ADD);
+				LOG_ERROR("SOUNDMANAGER", "Given sound name already exists:", sound->getName().c_str());
+				return;
+			}
+		}
 		m_sounds.push_back(sound);
 	}
 
@@ -27,6 +36,7 @@ namespace greet{ namespace audio {
 		for (uint i = 0; i < size; i++)
 			if (soundname.compare(m_sounds[i]->getName().c_str()) == 0)
 				return m_sounds[i];
+		utils::ErrorHandle::setErrorCode(GREET_ERROR_MANAGER_GET);
 		LOG_ERROR("SOUNDMANAGER","Could not find the given sound: ", soundname.c_str());
 		return m_sounds[0];
 	}
--- Greet-core/src/graphics/textures/texturemanager.cpp
+++ Greet-core/src/graphics/textures/texturemanager.cpp
@@ -6,6 +6,15 @@ namespace greet{ namespace graphics{
 
 	void TextureManager::add(graphics::Texture* texture)
 	{
+		for (int i = 0;i < m_textures.size();i++)
+		{
+			if (texture->getName() == m_textures[i]->getName())
+			{
+				utils::ErrorHandle::setErrorCode(GREET_ERROR_MANAGER_ADD);
+				LOG_ERROR("TEXTUREMANAGER", "Given texture name already exists:", texture->getName().c_str());
+				return;
+			}
+		}
 		m_textures.push_back(texture);
 	}
 
@@ -19,6 +28,7 @@ namespace greet{ namespace graphics{
 				return m_textures[i];
 			}
 		}
+		utils::ErrorHandle::setErrorCode(GREET_ERROR_MANAGER_GET);
 		LOG_ERROR("TEXTUREMANAGER","Could not find the given texture: ", texturename.c_str());
 		return m_textures[0];
 	}
