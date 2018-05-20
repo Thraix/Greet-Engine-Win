41ffbd2d958ac5e82f6529f8348fc1a866a43887
Tim Håkansson <thraix@hotmail.com>
2018-05-20 10:36:53 +0200

    Add GreetFormatting submodule

--- examples/GUIPlayground/src/Theme.h
+++ examples/GUIPlayground/src/Theme.h
@@ -1,6 +1,12 @@
 #pragma once
 
+#include "Property.h"
+#include <map>
+
 struct Theme
 {
+	std::map<std::string, std::string> widthProperties; // 
+	std::map<std::string, std::string> heightProperties;
+
 
 };
